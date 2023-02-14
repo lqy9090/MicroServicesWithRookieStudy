ddcfd6df4aa9cb858ed0f5d4df6406e0f422a5f7
lqy9090 <68727327+lqy9090@users.noreply.github.com>
2023-02-14 16:25:15 +0800

    Week3 actuator

---
+++ customer/src/main/java/com/quinn/customer/security/ActuatorSecurity.java
@@ -0,0 +1,25 @@
+package com.quinn.customer.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Author: qiuyi
+ * @Description:
+ * @DateTime: 2023/2/9 19:45
+ **/
+@Configuration
+@EnableWebSecurity
+public class ActuatorSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requestMatcher(EndpointRequest.toAnyEndpoint()).authorizeRequests()
+                .anyRequest().authenticated().and().httpBasic();
+    }
+
+}
