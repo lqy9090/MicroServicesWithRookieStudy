5eaf9495a39eea923454b41661b703a558419721
lqy9090 <68727327+lqy9090@users.noreply.github.com>
2023-02-19 17:37:15 +0800

    Week3 scheduled tasks fixed

---
+++ asyncMethods/src/main/java/com/quinn/asyncmethod/systemLearning/ThreadUnsafeExample.java
@@ -0,0 +1,14 @@
+package com.quinn.asyncmethod.systemLearning;
+
+public class ThreadUnsafeExample {
+
+    private int cnt = 0;
+
+    public void add() {
+        cnt++;
+    }
+
+    public int get() {
+        return cnt;
+    }
+}
